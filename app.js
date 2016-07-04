7496098b0ea6b905210e08e10cd2ffa63f490f3a
Marc <mjcuva@gmail.com>
2016-07-04 15:27:16 +0700

    Enable cors

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', api);
 app.use('/login', login);
 
